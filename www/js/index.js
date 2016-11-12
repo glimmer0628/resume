820e28d1c2a0f1354e764aaf915deb4a0a482052
lihuihui <845055342@qq.com>
2016-11-12 15:44:24 +0800

    第三次
    aa

--- www/js/index.js
+++ www/js/index.js
@@ -100,7 +100,7 @@
 	            // var b_col=['palegreen','#04A3E9','deeppink','orangered'];
 	             // $("#scroller_list").append("<li class='skill_title'>专业技能</li>");
 	              $("#scroller_list").css('background','transparent');
-	             $("#scroller_list").append("<li ><div class='skill_title'><img src='../images/7.png'/><img src='../images/8.png'/><img src='../images/9.png'/></div></li>")
+	             $("#scroller_list").append("<li ><div class='skill_title'><img src='images/7.png'/><img src='images/8.png'/><img src='images/9.png'/></div></li>")
 	            for(var i=0;i<response.length;i++){
 	                $("#scroller_list").append("<li class='skill_li'><div class='skill_left skill_left"+i+"'></div><div class='skill_right'><span class='cg'>"+response[i].category+"</span><span><span class='skill_sp'>设计内容：</span>"+
 	                    response[i].name+"</span><span><span class='skill_sp'>掌握程度：</span>"+
@@ -142,7 +142,7 @@
 	            for(var i=0;i<response.length;i++){
 	                $("#scroller_list").append("<li class='project_li'><span class='cg'>"+
 	                    response[i].name+"<br/><span>"+ response[i].category+
-	                    "</span></span><img src='../images/1.jpg'/><span><span class='pro_sp'>项目描述：</span>"+
+	                    "</span></span><img src='images/1.jpg'/><span><span class='pro_sp'>项目描述：</span>"+
 	                    response[i].description+"</span><span><span class='pro_sp'>负责模块：</span>"+
 	                    response[i].detail+"</span><span><span class='pro_sp'>使用技术：</span>"+
 	                    response[i].tech+"</span></li>")
@@ -167,7 +167,7 @@
 	            response=JSON.parse(response);
 	            // $("#scroller_list").append("<li class='work_title'>工作经历</li>")
 	             $("#scroller_list").css('background','#CFECFB');
-	            $("#scroller_list").append("<li ><div class='work_title'><img src='../images/10.png'/><img src='../images/11.png'/><img src='../images/12.png'/></div></li>")
+	            $("#scroller_list").append("<li ><div class='work_title'><img src='images/10.png'/><img src='images/11.png'/><img src='images/12.png'/></div></li>")
 	           
 	            for(var i=0;i<response.length;i++){
 	                
@@ -193,13 +193,25 @@
 	        $("#scroller_list").append("<li class='my_li1'><span class='my_title'>个人基本情况</span><span class='title_e' >ABOUT ME</span>");
 	        // $("#scroller_list").append("<li class='title_e' >ABOUT ME</li>");
 	        $("#scroller_list").append("<li class='my_li'><span><span class='sp_t'>期望职位: </span>前端开发工程师</span><span><span class='sp_t'>期望月薪: </span>8k-10k</span><span ><span class='sp_t'>工作地点: </span>北京、青岛</span><span ><span class='sp_t'>自我评价：</span>我是一个做事认真负责，有耐心，能吃苦的人。平时工作会有自己的idea，创新能力比较强，有很强的团队意识，善与人沟通。希望进入贵公司，与公司一起成长进步。</span></li>");
-	         $("#scroller_list").append("<li><img class='my_img' src='../images/6.png' /></li>");
+	         $("#scroller_list").append("<li><img class='my_img' src='images/6.png' /></li>");
 	       })
 	   
 	})
 
 
+	var interval=setInterval(function(){
+	    if(document.readyState==='complete'){
+	        clearInterval(interval);
+	        $('#preload').hide();
+	        $('.swiper-container').show();
+	        swiper.updateContainerSize();
+	        swiper.updateSlidesSize();
+	        
 
+	    }else{
+	        $('#preload').show();
+	    }
+	},50);
 
 /***/ },
 /* 2 */
